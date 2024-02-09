0449a69082bb9221e0be7993e3c30a445fdf963e
codershisho <codershisho@gmail.com>
2024-02-09 23:51:25 +0900

    vuetify導入

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>aaaabbbbb</div>
+  <v-btn color="primary">aaaa</v-btn>
 </template>
 
 <script setup lang="ts"></script>
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,6 +3,14 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+// vuetify
+import { createVuetify } from 'vuetify';
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/dist/vuetify.min.css';
+const vuetify = createVuetify();
+
 const app = createApp(App);
+
+app.use(vuetify);
 
 app.mount('#app');
